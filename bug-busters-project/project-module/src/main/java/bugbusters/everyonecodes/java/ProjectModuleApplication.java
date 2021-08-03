e8b30ba12838fb666da6cfc2da1dc39c96ddf1db
LeSchoki <georg.soher@gmail.com>
2021-08-03 10:55:30 +0200

    Module set up

---
+++ bug-busters-project/project-module/src/main/java/bugbusters/everyonecodes/java/ProjectModuleApplication.java
@@ -0,0 +1,13 @@
+package bugbusters.everyonecodes.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectModuleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectModuleApplication.class, args);
+    }
+
+}
---
+++ bug-busters-project/project-module/src/test/java/bugbusters/everyonecodes/java/ProjectModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package bugbusters.everyonecodes.java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectModuleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
