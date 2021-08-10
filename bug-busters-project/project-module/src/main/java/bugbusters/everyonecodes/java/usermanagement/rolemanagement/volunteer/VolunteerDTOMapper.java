1841ab1c9041979544353b6c3d1d675c55d828e6
Nadya Plank <nadyaplank@gmail.com>
2021-08-10 10:52:14 +0200

    added VolunteerDTOMapper

--- bug-busters-project/project-module/src/main/java/bugbusters/everyonecodes/java/usermanagement/rolemanagement/volunteer/VolunteerDTOMapper.java
+++ bug-busters-project/project-module/src/main/java/bugbusters/everyonecodes/java/usermanagement/rolemanagement/volunteer/VolunteerDTOMapper.java
@@ -1,4 +1,25 @@
 package bugbusters.everyonecodes.java.usermanagement.rolemanagement.volunteer;
 
+import bugbusters.everyonecodes.java.usermanagement.service.UserDTOMapper;
+
 public class VolunteerDTOMapper {
+    private final UserDTOMapper userDTOMapper;
+    private final SetToStringMapper setToStringMapper;
+
+    public VolunteerDTOMapper(UserDTOMapper userDTOMapper, SetToStringMapper setToStringMapper) {
+        this.userDTOMapper = userDTOMapper;
+        this.setToStringMapper = setToStringMapper;
+    }
+
+    VolunteerPrivateDTO toPrivate(Volunteer input) {
+        var user = userDTOMapper.toUserPrivateDTO(input.getUser());
+        var skills = setToStringMapper.convertToString(input.getSkills());
+        return new VolunteerPrivateDTO(user, skills);
+    }
+
+    VolunteerPublicDTO toPublic(Volunteer input) {
+        var user = userDTOMapper.toUserPublicDTO(input.getUser());
+        var skills = setToStringMapper.convertToString(input.getSkills());
+        return new VolunteerPrivateDTO(user, skills);
+    }
 }
--- bug-busters-project/project-module/src/main/java/bugbusters/everyonecodes/java/usermanagement/rolemanagement/volunteer/VolunteerPublicDTO.java
+++ bug-busters-project/project-module/src/main/java/bugbusters/everyonecodes/java/usermanagement/rolemanagement/volunteer/VolunteerPublicDTO.java
@@ -1,7 +1,9 @@
 package bugbusters.everyonecodes.java.usermanagement.rolemanagement.volunteer;
 
 import bugbusters.everyonecodes.java.usermanagement.data.UserPublicDTO;
+import com.fasterxml.jackson.annotation.JsonInclude;
 
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class VolunteerPublicDTO {
     private UserPublicDTO user;
     private String skills;
