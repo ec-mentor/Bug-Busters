40756e97c85b33aa9478361337102190641a0f74
LeSchoki <georg.soher@gmail.com>
2021-08-16 13:12:20 +0200

    Implemented Search for individual, extended webapptree for org and indv

--- bug-busters-project/project-module/src/main/java/bugbusters/everyonecodes/java/usermanagement/rolemanagement/individual/IndividualEndpoint.java
+++ bug-busters-project/project-module/src/main/java/bugbusters/everyonecodes/java/usermanagement/rolemanagement/individual/IndividualEndpoint.java
@@ -3,12 +3,14 @@ package bugbusters.everyonecodes.java.usermanagement.rolemanagement.individual;
 import bugbusters.everyonecodes.java.usermanagement.rolemanagement.organization.ClientPrivateDTO;
 import bugbusters.everyonecodes.java.usermanagement.rolemanagement.organization.ClientPublicDTO;
 import bugbusters.everyonecodes.java.usermanagement.rolemanagement.volunteer.VolunteerPublicDTO;
+import bugbusters.everyonecodes.java.usermanagement.rolemanagement.volunteer.VolunteerSearchResultDTO;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping("/individual")
@@ -39,6 +41,16 @@ public class IndividualEndpoint {
     @GetMapping("/view/{username}")
     VolunteerPublicDTO viewVolunteerPublicDat(@PathVariable String username) {
         return individualService.viewVolunteerPublicData(username).orElse(null);
+    }
+
+    @GetMapping("/view/volunteers")
+    List<VolunteerSearchResultDTO> listAllVolunteers() {
+        return individualService.listAllVolunteers();
+    }
+
+    @GetMapping("/search/{text}")
+    List<VolunteerSearchResultDTO> searchVolunteersByText(@PathVariable String text) {
+        return individualService.searchVolunteersByText(text);
     }
 
     @GetMapping("/webapptree")
