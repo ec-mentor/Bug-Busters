f130392025e28c984bf038fcfb414f3d708e9f8e
LAPTOP-CDQEB8A1\Ratko <Josic.Ratko@gmx.at>
2021-08-16 12:43:29 +0200

    ActivityEndpoint

--- bug-busters-project/project-module/src/main/java/bugbusters/everyonecodes/java/activities/ActivityEndpoint.java
+++ bug-busters-project/project-module/src/main/java/bugbusters/everyonecodes/java/activities/ActivityEndpoint.java
@@ -1,14 +1,37 @@
 package bugbusters.everyonecodes.java.activities;
 
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
+@RestController
+@RequestMapping("/activities")
 public class ActivityEndpoint {
+    private final ActivityService activityService;
 
-
-    @PostMapping
-    Activity createActivity(@RequestBody Activity activity) {
-        return activityService.save(activity);
+    public ActivityEndpoint(ActivityService activityService) {
+        this.activityService = activityService;
     }
+
+    @PostMapping("/post")
+    Activity postActivity(@RequestBody Activity activity) {
+        return activityService.postActivity(activity);
+    }
+
+    @PutMapping("/edit/{title}")
+    Activity editActivity(@RequestBody Activity activity, @PathVariable String title){
+        return activityService.edit(activity, title).orElse(null);
+    }
+
+    @GetMapping()
+    List<Activity> getAllActivities(){
+        return activityService.findAll();
+    }
+
+    @GetMapping("/{title}")
+    Activity getByTitle(@PathVariable String title){
+        return activityService.getActivityByTitle(title).orElse(null);
+    }
+
 
 }
