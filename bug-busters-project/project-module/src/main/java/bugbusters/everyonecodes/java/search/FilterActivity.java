e0313513b929ca82e8d21c319d0e86b1bc795ed8
LAPTOP-CDQEB8A1\Ratko <Josic.Ratko@gmx.at>
2021-08-23 14:37:14 +0200

    FilterActivity added

--- bug-busters-project/project-module/src/main/java/bugbusters/everyonecodes/java/search/FilterActivity.java
+++ bug-busters-project/project-module/src/main/java/bugbusters/everyonecodes/java/search/FilterActivity.java
@@ -1,4 +1,86 @@
 package bugbusters.everyonecodes.java.search;
 
+import java.time.LocalDateTime;
+import java.util.Objects;
+
 public class FilterActivity {
+    private LocalDateTime date;
+    private String category;
+    private String skills;
+    private String creator;
+    private Integer rating;
+
+    public FilterActivity(){}
+
+    public FilterActivity(LocalDateTime date, String category, String skills, String creator, Integer rating) {
+        this.date = date;
+        this.category = category;
+        this.skills = skills;
+        this.creator = creator;
+        this.rating = rating;
+    }
+
+    public LocalDateTime getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDateTime date) {
+        this.date = date;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getSkills() {
+        return skills;
+    }
+
+    public void setSkills(String skills) {
+        this.skills = skills;
+    }
+
+    public String getCreator() {
+        return creator;
+    }
+
+    public void setCreator(String creator) {
+        this.creator = creator;
+    }
+
+    public Integer getRating() {
+        return rating;
+    }
+
+    public void setRating(Integer rating) {
+        this.rating = rating;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        FilterActivity that = (FilterActivity) o;
+        return Objects.equals(date, that.date) && Objects.equals(category, that.category) && Objects.equals(skills, that.skills) && Objects.equals(creator, that.creator) && Objects.equals(rating, that.rating);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(date, category, skills, creator, rating);
+    }
+
+    @Override
+    public String toString() {
+        return "FilterActivity{" +
+                "date=" + date +
+                ", category='" + category + '\'' +
+                ", skills='" + skills + '\'' +
+                ", creator='" + creator + '\'' +
+                ", rating=" + rating +
+                '}';
+    }
 }
--- bug-busters-project/project-module/src/main/java/bugbusters/everyonecodes/java/search/FilterActivityService.java
+++ bug-busters-project/project-module/src/main/java/bugbusters/everyonecodes/java/search/FilterActivityService.java
@@ -1,4 +1,8 @@
 package bugbusters.everyonecodes.java.search;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class FilterActivityService {
+
 }
